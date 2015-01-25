2cdb877c5e548e0215d30c1800bd8127425013d4
Richard Joffray <richard@joffray.com>
2015-01-25 11:58:50 -0800

    alert added

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,7 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-
+alert("ready");
         var ref = window.open('http://apache.org', '_blank', 'location=yes');
 
         ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
